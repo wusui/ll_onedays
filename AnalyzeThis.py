3367df0db8a65711ac57bc2987e288f1e0484957
Warren Usui <wusui@users.noreply.github.com>
2018-01-21 23:30:52 -0800

    Lint changes

--- AnalyzeThis.py
+++ AnalyzeThis.py
@@ -1,58 +1,67 @@
 #!/usr/bin/python
+""" TO DO: """
 from html.parser import HTMLParser
 import requests
 
 class MatchParse(HTMLParser):
+    """ TO DO: """
     def __init__(self, player):
+        """ TO DO: """
         HTMLParser.__init__(self)
         self.iam = player
-        self.counter = 0
         self.results = []
         self.playerv = []
-        self.scanforPos = False
-        self.scanforName = False
-        self.scanforScores = False
-        self.right = 0
-        self.wrong = 0
-        self.commented = False
+        self.scan_for = {}
+        self.scan_for['pos'] = False
+        self.scan_for['name'] = False
+        self.scan_for['scores'] = False
+        self.scan_for['commented'] = False
+        self.lval = {}
+        self.lval['right'] = 0
+        self.lval['wrong'] = 0
+        self.lval['myposition'] = 0
+        self.lval['counter'] = 0
         self.currentdata = []
-        self.myPosition = 0
 
     def handle_starttag(self, tag, attrs):
-        if not self.scanforPos:
+        """ TO DO: """
+        if tag in []:
+            return
+        if not self.scan_for['pos']:
             for apt in attrs:
                 if apt[0] == 'class':
                     if apt[1] == 'std-midleft':
-                        self.counter += 1
-                        self.scanforPos = True
+                        self.lval['counter'] += 1
+                        self.scan_for['pos'] = True
     def handle_data(self, data):
-        if self.scanforScores:
+        """ TO DO: """
+        if self.scan_for['scores']:
             try:
                 nval = int(data)
             except ValueError:
                 print('%s is invalid numerical input' % data)
                 return
             if nval == 0:
-                self.wrong += 1
+                self.lval['wrong'] += 1
             else:
-                self.right += 1
-            if (self.right + self.wrong) == 12:
-                self.currentdata.append(self.right)
-                self.scanforScores = False
+                self.lval['right'] += 1
+            if (self.lval['right'] + self.lval['wrong']) == 12:
+                self.currentdata.append(self.lval['right'])
+                self.scan_for['scores'] = False
                 if self.currentdata[1].endswith(self.iam):
                     self.playerv = self.currentdata
-                    self.myPosition = self.currentdata[0]
+                    self.lval['myposition'] = self.currentdata[0]
                 self.results.append(self.currentdata)
-                self.right = 0
-                self.wrong = 0
+                self.lval['right'] = 0
+                self.lval['wrong'] = 0
                 self.currentdata = []
-        if self.scanforName:
-            if data.startswith('XXXXX:') or self.commented:
+        if self.scan_for['name']:
+            if data.startswith('XXXXX:') or self.scan_for['commented']:
                 self.currentdata.append(data)
-                self.scanforPos = False
-                self.scanforName = False
-                self.scanforScores = True
-        if self.scanforPos:
+                self.scan_for['pos'] = False
+                self.scan_for['name'] = False
+                self.scan_for['scores'] = True
+        if self.scan_for['pos']:
             if data.endswith('.'):
                 strval = data[0:-1]
                 try:
@@ -60,42 +69,29 @@ class MatchParse(HTMLParser):
                 except ValueError:
                     return
                 self.currentdata.append(posis)
-                self.scanforName = True
+                self.scan_for['name'] = True
 
-def AnalyzeThis(filen, player):
-    r = requests.get(filen)
+def analyze_this(filen, player):
+    """ TO DO: """
+    reqf = requests.get(filen)
     retv = False
     scanner = ['&nbsp;%s' % player, '&nbsp;-->%s' % player]
     for field in scanner:
-        if field in r.text:
+        if field in reqf.text:
             retv = True
             break
     if not retv:
         return False
     parser = MatchParse(player)
-    if r.text.find('&nbsp;-->') > 0:
-        parser.commented = True
-    our_version = r.text.split('&nbsp')
+    if reqf.text.find('&nbsp;-->') > 0:
+        parser.scan_for['commented'] = True
+    our_version = reqf.text.split('&nbsp')
     our_text = 'XXXXX:'.join(our_version)
     parser.feed(our_text)
     tie_count = 0
     total = 0
     for plyr in parser.results:
-        if plyr[0] == parser.myPosition:
+        if plyr[0] == parser.lval['myposition']:
             tie_count += 1
         total += 1
     return [parser.playerv, total, tie_count]
-
-if __name__ == '__main__':
-    testlist = ['https://learnedleague.com/oneday/results.php?endings&1',
-                'https://learnedleague.com/oneday/results.php?americanwildlife&1',
-                'https://learnedleague.com/oneday.php?kentucky',
-                'https://learnedleague.com/oneday.php?tennis',
-                'https://learnedleague.com/oneday/skiing.php',
-                'https://learnedLeague.com/oneday/paulmccartney.shmtl',
-                'https://learnedleague.com/oneday/results.php?classic_72&1',
-                'https://learnedleague.com/oneday.php?tourdefrance']
-    for filev in testlist:
-        from CommonStuff import HELLO_MY_NAME_IS
-        result = AnalyzeThis(filev, HELLO_MY_NAME_IS)
-        print(result)
