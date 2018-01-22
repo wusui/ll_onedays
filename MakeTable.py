8f0966469e3435b51bdd8c880b2df85912169229
Warren Usui <wusui@users.noreply.github.com>
2018-01-21 23:39:34 -0800

    Lint changes

--- MakeTable.py
+++ MakeTable.py
@@ -1,24 +1,26 @@
 #!/usr/bin/python
+""" TO DO: """
 HEADER_PARTS = ['<table border="1">',
-               '<tr><td> #Correct</td>',
-               '<td>Best Finish</td>',
-               '<td>Worst Finish</td>',
-               '</tr><br />']
+                '<tr><td> #Correct</td>',
+                '<td>Best Finish</td>',
+                '<td>Worst Finish</td>',
+                '</tr><br />']
 HEADER = ''.join(HEADER_PARTS)
 LINE_TEMPLATE = '<tr><td>%d</td><td>%d pctile</td><td>%d pctile</td></tr>'
 
-def MakeTable(hival, loval, outfile, use_brackets):
+def make_table(hival, loval, outfile, use_brackets):
+    """ TO DO: """
     fstring = HEADER
-    for i in range(1,13):
-        x = hival[i]['pctile']
-        y = loval[i]['pctile']
-        ostring = LINE_TEMPLATE % (i,x,y)
-        ostring = ostring.replace('pctile','%ile')
-        fstring = ''.join([fstring,  ostring])
+    for i in range(1, 13):
+        xrec = hival[i]['pctile']
+        yrec = loval[i]['pctile']
+        ostring = LINE_TEMPLATE % (i, xrec, yrec)
+        ostring = ostring.replace('pctile', '%ile')
+        fstring = ''.join([fstring, ostring])
     fstring = ''.join([fstring, '</table>'])
     if use_brackets:
         fstring = fstring.replace('<', '[')
         fstring = fstring.replace('>', ']')
         fstring = fstring.replace(' border="1"', '')
-    with open(outfile, 'w') as f:
-        f.write(fstring)
+    with open(outfile, 'w') as filedesc:
+        filedesc.write(fstring)
